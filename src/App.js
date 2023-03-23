cc7b8ac09c0939d804afc27c10af4712fc1a02f2
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-03-23 10:41:14 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 import "./App.css";
-import TaskScreen from "./screens/TaskScreen";
-
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const editTodo = (id, updatedTodo) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, task: updatedTodo };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const toggleCompleted = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="main">
-      <h1> Welcome to To Do List Application. </h1> <TaskScreen />{" "}
-      {/* This is like a function call */}{" "}
+    <div className="container mt-5">
+      <h1 className="text-center mb-5">To-Do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+        editTodo={editTodo}
+        toggleCompleted={toggleCompleted}
+      />
     </div>
   );
 }
 
 export default App;
-
-// What is a React Component?
-
-// A React Component is LIKE a function.
-// It takes in zero or a few arguments.
-// It always returns some UI in form of JSX.
-// A React Component can be represented by a JS class or a JS function.
-
-// JSX -> JavaScript XML
-// It is like HTML, but it also supports React Components
-// You can also write JS code inside JSX.
--- src/test.js
+++
@@ -1,8 +0,0 @@
-function addTwoNumbers(a, b) {
-  let sum = a + b;
-  let diff =  a - b;
-  return [sum, diff]
-}
-
-let [x, y] = addTwoNumbers(5, 6)
-console.log(x, y)
